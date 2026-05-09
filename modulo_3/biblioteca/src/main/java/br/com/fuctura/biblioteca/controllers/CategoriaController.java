2e4312492904ebfefe65d672108694ce52fff91c
hebertaragao <hebertaragao.dev@gmail.com>
2026-05-09 12:20:43 -0300

    Update

--- modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/controllers/CategoriaController.java
+++ modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/controllers/CategoriaController.java
@@ -1,4 +1,26 @@
 package br.com.fuctura.biblioteca.controllers;
 
+import br.com.fuctura.biblioteca.models.Categoria;
+import br.com.fuctura.biblioteca.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/{id}")
+    public Categoria findById(@PathVariable int id) {
+        Categoria cat = categoriaService.findById(id);
+        return cat;
+    }
+
 }
--- modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/services/CategoriaService.java
+++ modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/services/CategoriaService.java
@@ -1,4 +1,20 @@
 package br.com.fuctura.biblioteca.services;
 
+import br.com.fuctura.biblioteca.models.Categoria;
+import br.com.fuctura.biblioteca.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria findById(Integer id) {
+        Optional<Categoria> cat = categoriaRepository.findById(id);
+        return cat.get();
+    }
 }
