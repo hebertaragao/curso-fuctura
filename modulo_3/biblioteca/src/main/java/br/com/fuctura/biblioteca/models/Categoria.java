7d986107f55941bd5039a2914fd03da8d5d1f8e1
hebertaragao <hebertaragao.dev@gmail.com>
2026-05-09 10:43:28 -0300

    Criado data.sql

--- modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/models/Categoria.java
+++ modulo_3/biblioteca/src/main/java/br/com/fuctura/biblioteca/models/Categoria.java
@@ -1,11 +1,17 @@
 package br.com.fuctura.biblioteca.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+//@AllArgsConstructor
 public class Categoria {
 
 	@Id
@@ -17,37 +23,9 @@ public class Categoria {
 	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
-	public Categoria() {
-	}
-
 	public Categoria(Integer id, String nome, String descricao) {
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
 }
